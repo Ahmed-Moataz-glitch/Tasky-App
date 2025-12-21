80793467a1577e80f75aae52b8c787c175324606
Ahmed Moataz <ahmedmoataz221104@gmail.com>
2025-12-21 15:38:00 +0200

    add safe area to onboarding page

--- lib/views/pages/onboarding_page.dart
+++ lib/views/pages/onboarding_page.dart
@@ -17,99 +17,106 @@ class _OnboardingPageState extends State<OnboardingPage> {
     return Scaffold(
       backgroundColor: Color(0xffffffff),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            children: [
-              SizedBox(height: 150),
-              SizedBox(
-                width: 375,
-                height: 240,
-                child: PageView.builder(
-                  controller: pageController,
-                  itemBuilder: (context, index) {
-                    return Image.asset(
-                      onboardingPages[currentIndex].imagePath,
-                      width: 375,
-                      height: 240,
-                    );
-                  },
-                  onPageChanged: (index) {
-                    setState(() {
-                      currentIndex = index;
-                    });
-                  },
-                  itemCount: onboardingPages.length,
-                ),
-              ),
-              const SizedBox(height: 24),
-              SmoothPageIndicator(
-                controller: pageController,
-                count: onboardingPages.length,
-                effect: ExpandingDotsEffect(
-                  activeDotColor: Color(0xff5F33E1),
-                  dotColor: Color(0xffAFAFAF),
-                  dotHeight: 8,
-                  dotWidth: 8,
-                  spacing: 10,
-                ),
-              ),
-              SizedBox(height: 50),
-              Text(
-                onboardingPages[currentIndex].title,
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xDE24252C),
-                ),
-              ),
-              SizedBox(height: 42),
-              Text(
-                onboardingPages[currentIndex].description,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xDE6E6A7C),
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 107),
-              Row(
-                children: [
-                  const Spacer(),
-                  ElevatedButton(
-                    onPressed: () {
-                      if(currentIndex < onboardingPages.length - 1){
-                        pageController.nextPage(
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeIn,
-                        );
-                        setState(() {
-                          currentIndex++;
-                        });
-                      } else {
-                        Navigator.of(context).pushNamed('/login');
-                      }
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                SizedBox(height: 150),
+                SizedBox(
+                  width: 375,
+                  height: 240,
+                  child: PageView.builder(
+                    controller: pageController,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        onboardingPages[currentIndex].imagePath,
+                        width: 375,
+                        height: 240,
+                      );
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff5F33E1),
-                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      currentIndex < onboardingPages.length - 1 ? 'NEXT' : 'GET STARTED',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xffffffff),
-                      ),
-                    ),
+                    onPageChanged: (index) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                    itemCount: onboardingPages.length,
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 24),
+                SmoothPageIndicator(
+                  controller: pageController,
+                  count: onboardingPages.length,
+                  effect: ExpandingDotsEffect(
+                    activeDotColor: Color(0xff5F33E1),
+                    dotColor: Color(0xffAFAFAF),
+                    dotHeight: 8,
+                    dotWidth: 8,
+                    spacing: 10,
+                  ),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  onboardingPages[currentIndex].title,
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xDE24252C),
+                  ),
+                ),
+                SizedBox(height: 42),
+                Text(
+                  onboardingPages[currentIndex].description,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xDE6E6A7C),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 107),
+                Row(
+                  children: [
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (currentIndex < onboardingPages.length - 1) {
+                          pageController.nextPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                          setState(() {
+                            currentIndex++;
+                          });
+                        } else {
+                          Navigator.of(context).pushNamed('/login');
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff5F33E1),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 12,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        currentIndex < onboardingPages.length - 1
+                            ? 'NEXT'
+                            : 'GET STARTED',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
